4ba5863ccf5d64b6474b22496f3ad56f3b6ea887
Esther Anierobi <esther.anierobi@gmail.com>
2022-06-22 11:17:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = 'Esther '
+let myHeight = '5ft11'
+let myCountry = " I'm from Nigeria."
+
+document.write('Hello!' + '<br><br>' +' Welcome to my Script' + '<br><br>')
+
+document.write('My name is ' + myName.toUpperCase() + ", <br><br> I've a height of " + myHeight + '. <br><br>' + myCountry )
